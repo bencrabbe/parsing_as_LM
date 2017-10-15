fcc6f0e785342773bded8e8b9005eabb45806634
BenC <benoit.crabbe@gmail.com>
2017-10-15 13:47:45 +0200

    added path

--- replicate_mikolov-kn5-ptb.sh
+++ replicate_mikolov-kn5-ptb.sh
@@ -1,3 +1,8 @@
+
+
+#SRILM replication of KN5
+export PATH=$(PATH)/srilm
+
 ngram-count -order 5 -text ptb/ptb.train_50w.txt -lm templm -kndiscount
 -interpolate -unk -gt3min 1 -gt4min 1
 ngram -ppl ptb/ptb_test.txt -lm templm -order 5 -unk
