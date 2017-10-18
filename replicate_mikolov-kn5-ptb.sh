5fce1c4efb6dc23bc553325272d9ef2b130c96fe
bencrabbe <benoit.crabbe@gmail.com>
2017-10-18 12:34:33 +0200

    last setup

--- replicate_mikolov-kn5-ptb.sh
+++ replicate_mikolov-kn5-ptb.sh
@@ -2,7 +2,7 @@
 
 #SRILM replication of Mikoklov-KN5 -> (PPL 142.5 where Mikolov reports 141.2) 
 export PATH=$PATH:$PWD/srilm/bin/i686-m64
-ORD=4 
+ORD=3
 
 ngram-count -order $ORD -text $PWD/ptb/ptb_train_50w.txt -lm templm -kndiscount -interpolate -unk -gt3min 1 -gt4min 1
 ngram -ppl $PWD/ptb/ptb_test.txt -lm templm -order $ORD -unk
