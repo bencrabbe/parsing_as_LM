cfb633c3e1496a31226a72e445d65d42082a68b6
bencrabbe <benoit.crabbe@gmail.com>
2017-10-20 21:30:03 +0200

    refact

--- keras_extra.py
+++ keras_extra.py
@@ -8,11 +8,86 @@ from keras.engine import Layer,InputSpec
 from keras.layers import Input,Embedding, Dense,Activation,Flatten
 from keras.optimizers import SGD
 from keras import backend as K
+ 
+def perplexity(ytrue,ypred):
+    """
+    computes the log likelihood of the model : assumes ytrue is one hot coded.
+    """
+    #avg_ll = K.mean(K.log(K.dot(ytrue,K.transpose(ypred))))
+    #return K.exp(-avg_ll)
+    #return K.exp(K.mean(K.log(K.categorical_crossentropy(ytrue,ypred)),axis=-1))
+    return K.mean(K.categorical_crossentropy(ytrue,ypred))
 
 
+class NumericSequentialGenerator:
+    """
+    Stores an encoded treebank as a list of sentences (= list of words)
+    and provides access to it with a python generator interface.
+    This generator is Designed for sequential models (RNN,LSTM,GRU).
+    @TODO: handle unk words (nothing implemented).
+    """
+    def __init__(self,X,Y,batch_size,nclasses,unk_word_code):
+        """
+        @param X,Y the encoded X,Y values as lists (of lists for both X and Y)
+        @param batch_size:size of generated data batches
+        @param nclasses: number of Y classes
+        @param unk_word_code : the integer xcode for unknwown words
+        """
+        assert(len(X)==len(Y))
+        assert(all(len(s)==len(t) for s,t in zip(X,Y)))
+        self.X,self.Y = X,Y
+        self.nclasses = nclasses
+        self.N = sum(len(s) for s in X)  #N is the number of tokens in the data
+        self.idxes = list(range(len(X))) #indexes the sentences
+        self.batch_size = batch_size     #number of tokens in a batch
+        self.start_idx = 0               #token indexing
+        self.unk_x_code = unk_word_code
+        self.x_counts(self.X)
+        self.generate_tok_representation()
+
+        
+    def generate_tok_representation(self):
+        """
+        generate token lists from lists of sentences
+        """
+        self.Xtoks = [ x for x in sentence for sentence in self.X ]
+        self.Ytoks = [ y for y in sentence for sentence in self.Y ]
+        
+          
+    def select_indexes(self):
+        """
+        Internal function for randomly selecting samples indexes.
+        """
+        end_idx = self.start_idx+self.batch_size
+        if end_idx > self.N:
+            shuffle(self.idxes)
+            self.generate_tok_representation()
+            self.start_idx = 0
+            end_idx = self.batch_size
+        sidx = self.start_idx
+        self.start_idx = end_idx
+        return (sidx,end_idx)
+
+    def generate(self):
+        """
+        A generator called by the fitting function.
+        @yield : an encoded subset of the data
+        """
+        while True:
+            start_idx,end_idx = self.select_indexes()
+            Y     = np.zeros((self.batch_size,self.nclasses))
+            X     = self.Xtoks[start_idx:end_idx]
+            yvals = self.Ytoks[start_idx:end_idx]
+
+            for i,y in enumerate(yvals):
+                Y[i,y] = 1.0
+                yield (X,Y)
+
+                
 class NumericDataGenerator:
     """
-    Stores an encoded treebank and provides it with a python generator interface
+    Stores an encoded treebank and provides it with a python generator interface.
+    This generator is Designed for non sequential models (NNLM).
     """
     def __init__(self,X,Y,batch_size,nclasses,unk_word_code):
         """
@@ -40,7 +115,7 @@ class NumericDataGenerator:
     #you might want to avoid resampling reserved tokens such as START OR END SENTENCE markers
     def sample_one(self,x,alpha):
         """
-        Sample x data for unk words
+        Samples an x datum for unk words
         """
         f = alpha / (self.word_freqs[x]+alpha)
         if rand() < f:
@@ -87,9 +162,19 @@ class NumericDataGenerator:
             elif gen_oov < oov_rate:
                 amin = alpha
         return alpha
-                
-                
-    
+
+    def empty(self):
+        """
+        Boolean indicating if all data has been generated
+        """
+        return self.start_idx==self.N
+
+    def y_references_generated():
+        """
+        
+        """
+        return self.Y[start_idx:end_idx] 
+        
     def select_indexes(self,randomize=True):
         """
         Internal function for selecting samples indexes.
--- nnlm.py
+++ nnlm.py
@@ -8,9 +8,9 @@ import pandas as pd
 from math import log2,exp
 from numpy.random import choice,rand
 from keras.models import Sequential,load_model
-from keras.layers import Dense, Activation, Embedding,Flatten, Dropout
-from keras_extra import EmbeddingTranspose,NumericDataGenerator
-from keras.optimizers import RMSprop,Adam
+from keras.layers import Dense, Activation, Embedding,Flatten, Dropout,LSTM,GRU
+from keras_extra import EmbeddingTranspose,NumericDataGenerator,perplexity
+from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint,ReduceLROnPlateau
             
 class DependencyTree:
@@ -106,7 +106,6 @@ class DependencyTree:
         """
         return self.tokens[idx]
         
-
 class NNLanguageModel:
     """
     A simple NNLM a la Bengio 2002.
@@ -205,7 +204,22 @@ class NNLanguageModel:
                 
         return NumericDataGenerator(X,Y,batch_size,self.lexicon_size,self.word_codes[NNLanguageModel.UNKNOWN_TOKEN])
 
-
+    def make_sequential_data_generator(self):
+        """
+        This returns a data generator suitable for use with Keras
+        sequential models
+        @param treebank: the treebank to encode
+        @param batch_size: the size of the batches yielded by the generator
+        """
+        Y    = []
+        X    = []
+        for line in treebank:
+            tokens = [NNLanguageModel.UNKNOWN_TOKEN]+line+[NNLanguageModel.EOS_TOKEN]
+            X.append(tokens[:-1])
+            Y.append(tokens[1:])
+                
+        return NumericSequentialGenerator(X,Y,batch_size,self.lexicon_size,self.word_codes[NNLanguageModel.UNKNOWN_TOKEN])
+    
     
     def predict(self,sentence,yvalue=None):
         """
@@ -240,33 +254,20 @@ class NNLanguageModel:
             records.append( ( sentence[idx], sentence[idx] not in self.word_codes, preds[idx,yref],log2(preds[idx,yref])))
         return pd.DataFrame(records,columns=['token','unk_word','cond_prob','cond_log2prob'])
     
-            
-    def perplexity(self,treebank,uniform=False):
-        """
-        Computes the perplexity of an LM on a treebank
-        @param uniform : outputs a perplexity that would be produced
-        by an uniform distribution.
-        """
-        cross_entropy = 0
-        N             = 0
+    # def perplexity(self,treebank,uniform=False):
+    #     """
+    #     Computes the perplexity of an LM on a treebank
+    #     @param uniform : outputs a perplexity that would be produced
+    #     by an uniform distribution.
+    #     """
+    #     if uniform:
+    #        return self.lexicon_size
+    #     else:
+    #         xgen = self.make_data_generator(treebank,batch_size,test=True)
+    #         val = self.model.evaluate_generator(xgen,xgen.n_test_steps())
+    #         #return exp(val/xgen.N())
 
-        uniform_prob  = 1/len(self.word_codes)
-        for sentence in treebank:
-            X = []
-            Y = []
-            tokens = [NNLanguageModel.UNDEF_TOKEN,NNLanguageModel.UNDEF_TOKEN,NNLanguageModel.UNDEF_TOKEN]+sentence+[NNLanguageModel.EOS_TOKEN]
-            for (w3,w2,w1,y) in zip(tokens,tokens[1:],tokens[2:],tokens[3:]):
-                x,y = self.make_representation([w3,w2,w1],y)
-                X.append(x)
-                Y.append(y)
-            if uniform:
-                 cross_entropy += sum(log2(uniform_prob) for _ in range(len(Y)))
-            else:
-                preds = self.model.predict(X)
-                cross_entropy += sum( log2(preds[idx,yref]+np.finfo(float).eps) for idx,yref in enumerate(Y) )
-            N += len(Y)
-        return 2**(-cross_entropy/N)
-    
+            
     def sample_sentence(self):
 
         sentence = [NNLanguageModel.UNDEF_TOKEN,NNLanguageModel.UNDEF_TOKEN,NNLanguageModel.UNDEF_TOKEN]
@@ -280,6 +281,80 @@ class NNLanguageModel:
             action = self.predict(sentence)
         return sentence
 
+    def train_rnn_lm(self,\
+                    treebank_train,\
+                    treebank_validation,\
+                    lr=0.001,\
+                    hidden_dropout=0.1,\
+                    batch_size=100,\
+                    max_epochs=100,\
+                    alpha_lex=-1,\
+                    glove_file=None,
+                    lstm=True,
+                    gru=False,
+                    tied=True):
+        """
+        Trains an RNN word language model (Lstm or Gru) 
+        @param treebank_train        : a list of sentences
+        @param treebank_validation   : a list of sentences
+        @param lstm : bool, if True trains an LSTM (sets GRU to False)
+        @param gru  : bool, if True trains a GRU   (sets LSTM to False)
+        @param tied : bool, if True, ties input embedding layer with output word layer
+        """
+        #(1) build dictionaries
+        self.code_symbols(treebank_train) 
+        print("Dictionaries built.")
+
+        #(2) read off treebank and build keras data set
+        print("Encoding dataset from %d sentences."%len(treebank_train))
+        training_generator = self.make_data_generator(treebank_train,batch_size)
+        validation_generator = self.make_data_generator(treebank_validation,batch_size)
+        if alpha_lex < 0:
+            alpha_lex = training_generator.guess_alpha(validation_generator.oov_rate())
+    
+        print(self)
+        print("training examples [N] = %d\nBatch size = %d\nDropout = %f\nlearning rate = %f\nalpha-lex = %f"%(training_generator.N,batch_size,hidden_dropout,lr,alpha_lex))
+
+        #(3) Model structure
+        self.model = Sequential()
+        input_embedding = None
+        
+        if glove_file == None:
+            input_embedding = Embedding(self.lexicon_size,self.embedding_size,input_length=1)
+            self.model.add( input_embedding )
+        else:
+            input_embedding = Embedding(self.lexicon_size,\
+                                        self.embedding_size,\
+                                        input_length=1,\
+                                        trainable=True,\
+                                        weights=[self.read_glove_embeddings(glove_file)]) 
+        self.model.add(input_embedding)
+        self.model.add(Flatten())       #reshape the embedding layer
+        if lstm:
+            self.model.add(LSTM(self.hidden_size))
+        else:
+            self.model.add(GRU(self.hidden_size))
+        if hidden_dropout > 0:
+            self.model.add(Dropout(hidden_dropout))
+            #self.model.add(Dense(self.lexicon_size))
+        self.model.add(EmbeddingTranspose(self.lexicon_size,input_embedding))
+        self.model.add(Activation('softmax'))    
+
+        #(4) Fitting
+        sgd = Adam(lr=lr)  #sgd = Adam(lr=lr,beta_1=0.0)
+        self.model.compile(optimizer=sgd,loss='categorical_crossentropy',metrics=['accuracy'])
+        lr_scheduler = ReduceLROnPlateau( monitor='val_loss',factor=0.1,patience=5,verbose=1)
+        checkpoint = ModelCheckpoint('temporary-model-{epoch:02d}.hdf5', monitor='val_loss', verbose=0, save_best_only=True, mode='min')
+        ntrain_batches = max(1,round(training_generator.N/batch_size))
+        nvalidation_batches = max(1,round(validation_generator.N/batch_size))
+        log = self.model.fit_generator(generator = training_generator.generate(alpha=alpha_lex),\
+                                        validation_data = validation_generator.generate(),\
+                                        epochs=max_epochs,\
+                                        steps_per_epoch = ntrain_batches,\
+                                        validation_steps = nvalidation_batches,\
+                                        callbacks = [checkpoint,lr_scheduler])
+        return pd.DataFrame(log.history)
+    
     def train_nn_lm(self,\
                     treebank_train,\
                     treebank_validation,\
@@ -292,7 +367,8 @@ class NNLanguageModel:
                       
         """
         Locally trains a model with a static oracle and a standard feedforward NN.  
-        @param treebank : a list of dependency trees
+        @param treebank_train        : a list of sentences
+        @param treebank_validation   : a list of sentences
         """
         #(1) build dictionaries
         self.code_symbols(treebank_train) 
@@ -331,9 +407,10 @@ class NNLanguageModel:
         #self.model.add(Dense(self.lexicon_size))
         self.model.add(EmbeddingTranspose(self.lexicon_size,input_embedding))
         self.model.add(Activation('softmax'))
-        sgd = Adam(lr=lr,beta_1=0.0)
-        self.model.compile(optimizer=sgd,loss='categorical_crossentropy',metrics=['accuracy'])         #also works fine with adam
+
         #(4) Fitting
+        sgd = Adam(lr=lr)  #sgd = Adam(lr=lr,beta_1=0.0)
+        self.model.compile(optimizer=sgd,loss='categorical_crossentropy',metrics=['accuracy',perplexity])
         lr_scheduler = ReduceLROnPlateau( monitor='val_loss',factor=0.1,patience=5,verbose=1)
         checkpoint = ModelCheckpoint('temporary-model-{epoch:02d}.hdf5', monitor='val_loss', verbose=0, save_best_only=True, mode='min')
         ntrain_batches = max(1,round(training_generator.N/batch_size))
@@ -463,8 +540,10 @@ if __name__ == '__main__':
     lm = NNLanguageModel()
     lm.hidden_size    = 300
     lm.embedding_size = 300
-    lm.train_nn_lm(ttreebank,dtreebank,lr=0.00001,alpha_lex=0,hidden_dropout=0.3,batch_size=128,max_epochs=240,\
+    lm.train_nn_lm(ttreebank[:30],dtreebank[:30],lr=0.001,alpha_lex=0,hidden_dropout=0.3,batch_size=128,max_epochs=20,\
                     glove_file='glove/glove.6B.300d.txt')
+    #  lm.train_rnn_lm(ttreebank,dtreebank,lr=0.00001,alpha_lex=0,hidden_dropout=0.3,batch_size=128,max_epochs=240,\
+                    #glove_file='glove/glove.6B.300d.txt')
     lm.save_model('testLM')
     #lm = NNLanguageModel.load_model('testLM')
     #print('PPL-T = ',lm.perplexity(ttreebank),'PPL-D = ',lm.perplexity(dtreebank),'PPL-D(control) = ',lm.perplexity(dtreebank,uniform=True))
