c54aaf092e13c903e7602941997e037e78350312
bencrabbe <benoit.crabbe@gmail.com>
2017-10-23 10:40:57 +0200

    fixed loading/saving

--- nnlm.py
+++ nnlm.py
@@ -462,14 +462,12 @@ if __name__ == '__main__':
     #NNLanguageModel.grid_search(ttreebank,dtreebank,LR=[0.001],HSIZE=[200],ESIZE=[300])    
 
     lm = NNLanguageModel(hidden_size=300,embedding_size=300,input_length=3,tiedIO=True)
-    lm.train_nn_lm(ttreebank[:10],dtreebank[:10],lr=0.01,hidden_dropout=0.3,batch_size=512,max_epochs=30,glove_file='glove/glove.6B.300d.txt')
+    lm.train_nn_lm(ttreebank,dtreebank,lr=0.00001,hidden_dropout=0.4,batch_size=512,max_epochs=350,glove_file='glove/glove.6B.300d.txt')
     lm.save_model('final_model')
 
-    lm2 =  NNLanguageModel.load_model('final_model')
-
-    
-    for s in ttreebank[:5]:
-        print(lm2.predict_sentence(s))
+    #lm2 =  NNLanguageModel.load_model('final_model')
+    #for s in ttreebank[:5]:
+    #    print(lm2.predict_sentence(s))
 
     #for _ in range(10):
     #    print(' '.join(lm.sample_sentence()))
