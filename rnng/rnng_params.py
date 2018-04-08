1461bab65ff9eb1632826dc5cdfc33c43df2ac78
bencrabbe <benoit.crabbe@gmail.com>
2018-04-08 21:06:21 +0200

    softmax underflow fix

--- rnng/rnng_params.py
+++ rnng/rnng_params.py
@@ -3,7 +3,7 @@
 #These are namespaces for the hyperparams of the RNNG parser
 class StructParams:
 
-    OUTER_HIDDEN_SIZE  = 180
+    OUTER_HIDDEN_SIZE  = 256
     STACK_EMB_SIZE     = 256
     STACK_HIDDEN_SIZE  = 256 
     
@@ -11,5 +11,5 @@ class StructParams:
 class TrainingParams:
     LEX_MAX_SIZE      = 1000
     NUM_EPOCHS        = 1
-    LEARNING_RATE     = 0.01
+    LEARNING_RATE     = 0.1
     DROPOUT           = 0.3
