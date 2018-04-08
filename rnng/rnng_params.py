51a215c5690dbb439854469117f91eb27b4ee9ae
bencrabbe <benoit.crabbe@gmail.com>
2018-04-08 14:47:12 +0200

    added hyperparams file

---
+++ rnng/rnng_params.py
@@ -0,0 +1,15 @@
+
+
+#These are namespaces for the hyperparams of the RNNG parser
+
+class StructuralParams:
+
+    H_OUTER_HIDDEN_SIZE  = 250
+    H_STACK_EMB_SIZE     = 100
+    H_STACK_HIDDEN_SIZE  = 100 
+    
+    
+class DataSetParams:
+    H_LEX_MAX_SIZE      = 10000
+    H_NUM_EPOCHS        = 15
+    H_LEARNING_RATE     = 0.001
