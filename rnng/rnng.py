dbd89637365472b2ba60950e69230ee4284f09db
bencrabbe <benoit.crabbe@gmail.com>
2018-04-08 19:31:54 +0200

    softmax underflow fix

--- rnng/rnng.py
+++ rnng/rnng.py
@@ -318,7 +318,7 @@ class RNNGparser:
         btop = dy.parameter(self.preds_bias)
         bbot = dy.parameter(self.merge_bias)        
         probs = dy.softmax( (Wtop * dy.tanh((Wbot *stack_state.output()) + bbot)) + btop)
-        return np.maximum(probs.npvalue(),np.finfo(float).eps) * self.next_action_mask(configuration,last_action,sentence)
+        return np.maximum(probs.npvalue(),np.finfo(float32).eps) * self.next_action_mask(configuration,last_action,sentence)
         #this last line attempts to address numerical undeflows (0 out of dynet softmaxes) and applies the hard constraint mask
         #such that a legal action has a prob > 0.
     
