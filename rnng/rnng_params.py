f9ac3694897f388a24261e34012b469730224ec0
bencrabbe <benoit.crabbe@gmail.com>
2018-04-09 13:28:47 +0200

    refining params

--- rnng/rnng_params.py
+++ rnng/rnng_params.py
@@ -5,11 +5,10 @@ class StructParams:
 
     OUTER_HIDDEN_SIZE  = 256
     STACK_EMB_SIZE     = 100
-    STACK_HIDDEN_SIZE  = 100 
-    
+    STACK_HIDDEN_SIZE  = 256 
     
 class TrainingParams:
-    LEX_MAX_SIZE      = 5000
+    LEX_MAX_SIZE      = 3000
     NUM_EPOCHS        = 5
     LEARNING_RATE     = 0.0001
     DROPOUT           = 0.0001
