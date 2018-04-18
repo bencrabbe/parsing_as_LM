e445587a9449a99d73baa56f711bb5b72a50a87b
bencrabbe <benoit.crabbe@gmail.com>
2018-04-18 20:50:31 +0200

    changed history representation

--- rnng/rnng_params.py
+++ rnng/rnng_params.py
@@ -9,6 +9,6 @@ class StructParams:
     
 class TrainingParams:
     LEX_MAX_SIZE      = 10000   #for full PTB training
-    NUM_EPOCHS        = 50 
+    NUM_EPOCHS        = 100 
     LEARNING_RATE     = 0.0001
     DROPOUT           = 0.3
