7b32f28f5cd7410fd6b3c6e616a6d6ffe1d61a44
bencrabbe <benoit.crabbe@gmail.com>
2018-04-18 19:16:16 +0200

    changed history representation

--- rnng/rnng.py
+++ rnng/rnng.py
@@ -402,7 +402,7 @@ class RNNGparser:
         S,B,n,stack_state,lab_state,local_score = configuration
 
         hist_1  = structural_history[-1]
-        hist_2  = structural_history[-2] if len(history) >= 2 else None
+        hist_2  = structural_history[-2] if len(structural_history) >= 2 else None
         
         if not B or not S or (hist_1 == RNNGparser.OPEN and hist_2 != RNNGparser.SHIFT):
             MASK += self.open_mask
