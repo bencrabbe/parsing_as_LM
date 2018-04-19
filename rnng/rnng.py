851b95712352387e91084302e72db7f693020c66
bencrabbe <benoit.crabbe@gmail.com>
2018-04-19 11:20:08 +0200

    code refactoring shrinking & clean up

--- rnng/rnng.py
+++ rnng/rnng.py
@@ -393,7 +393,7 @@ class RNNGparser:
         MASK = np.array([True] * len(self.actions))
         S,B,n,stack_state,lab_state,local_score = configuration
 
-        print(self.pretty_print_configuration(configuration))
+        #print(self.pretty_print_configuration(configuration))
         
         hist_1  = structural_history[-1]
         hist_2  = structural_history[-2] if len(structural_history) >= 2 else None
@@ -407,9 +407,9 @@ class RNNGparser:
         if not S or n == 0 or (hist_1 == RNNGparser.OPEN and hist_2 != RNNGparser.SHIFT):
             MASK *= self.close_mask
 
-        print (MASK)
+        #print (MASK)
         restr_list = [idx for idx,mval in enumerate(MASK) if mval]
-        print('*',[self.actions[idx] for idx in restr_list])
+        #print('*',[self.actions[idx] for idx in restr_list])
         return restr_list
     
         
