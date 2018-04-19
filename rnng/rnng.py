583b05cb417243004be90c906e3655bcf9598dfc
bencrabbe <benoit.crabbe@gmail.com>
2018-04-19 11:54:06 +0200

    code refactoring shrinking & clean up

--- rnng/rnng.py
+++ rnng/rnng.py
@@ -525,6 +525,9 @@ class RNNGparser:
         iscorrect = (correct_prediction == best_prediction)
         loss       = dy.pick(-log_probs,correct_prediction)
         loss_val   = loss.value()
+        if loss_val == -np.inf:
+            print('argh')
+            exit(1)
         if backprop:
             loss.backward()
             self.trainer.update()
