0e81e4f00e78070943fdf6429736fdc57dbc4372
bencrabbe <benoit.crabbe@gmail.com>
2018-04-19 11:51:15 +0200

    code refactoring shrinking & clean up

--- rnng/rnng.py
+++ rnng/rnng.py
@@ -517,8 +517,10 @@ class RNNGparser:
             log_probs = dy.log_softmax( (W * dy.dropout(dy.tanh(stack_state.output()),self.dropout)) + b,self.restrict_structural_actions(configuration,structural_history))
         else:
             log_probs = dy.log_softmax( (W * dy.tanh(stack_state.output())) + b,self.restrict_structural_actions(configuration,structural_history))
-        #if lab_state != RNNGparser.WORD_LABEL and lab_state != RNNGparser.NT_LABEL:
-        #    print('scores',np.exp(log_probs.npvalue()))
+        if lab_state != RNNGparser.WORD_LABEL and lab_state != RNNGparser.NT_LABEL:
+            print('scores',np.exp(log_probs.npvalue()))
+            if max(np.exp(log_probs.npvalue())) == -np.inf:
+                exit(1)
         best_prediction = np.argmax(log_probs.npvalue())
         iscorrect = (correct_prediction == best_prediction)
         loss       = dy.pick(-log_probs,correct_prediction)
