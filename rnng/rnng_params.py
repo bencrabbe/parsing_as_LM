8f8f574d4560d5b4cdb5e6ad0f7bbf2c4484c3b2
bencrabbe <benoit.crabbe@gmail.com>
2018-04-20 15:15:54 +0200

    Brown Wrapper finished

--- rnng/rnng_params.py
+++ rnng/rnng_params.py
@@ -9,6 +9,6 @@ class StructParams:
     
 class TrainingParams:
     LEX_MAX_SIZE      = 10000   #for full PTB training
-    NUM_EPOCHS        = 3
+    NUM_EPOCHS        = 20
     LEARNING_RATE     = 0.0001
     DROPOUT           = 0.3
