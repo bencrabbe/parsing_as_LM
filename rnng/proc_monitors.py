8ad3dc2e3c1f2904b5c6c4f971160711520e6045
bencrabbe <benoit.crabbe@gmail.com>
2018-04-20 19:05:15 +0200

    added tracking monitor

---
+++ rnng/proc_monitors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+This module implements monitoring tools for tracking measures of
+interest for cognitive modeling
+"""
+
+class AbstractMonitor(object):
+    """
+    That's the empty monitor. It does nothing but it defines the
+    interface one wants to subclass for getting measures 
+    """
