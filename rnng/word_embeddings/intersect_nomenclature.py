072d6c634d168b5642a2c4e23531449785196b27
bencrabbe <benoit.crabbe@gmail.com>
2018-04-21 11:01:15 +0200

    w2v filter

--- rnng/word_embeddings/intersect_nomenclature.py
+++ rnng/word_embeddings/intersect_nomenclature.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+import sys
+
 """
 This module intersects the nomenclature of a word embedding dictionary
 with an existing nomenclature, hence generating a lightweight dictionary.
