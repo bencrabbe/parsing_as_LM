0816a3eb6102ad908fe40ad8a13dee7d41757de3
bencrabbe <benoit.crabbe@gmail.com>
2018-04-21 22:57:18 +0200

    added tracker

--- rnng/rnng.py
+++ rnng/rnng.py
@@ -1086,8 +1086,8 @@ if __name__ == '__main__':
     brown_file = None
     embedding_file = None 
     raw_file   = ''
-    lex_beam   = 1  #40
-    struct_beam = 1 #400
+    lex_beam   = 10  #40
+    struct_beam = 100 #400
     
     for opt, arg in opts:
         if opt in ['-h','--help']:
