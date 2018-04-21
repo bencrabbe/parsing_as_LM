7a409d1af6858615270adfcec41bebf25b056488
bencrabbe <benoit.crabbe@gmail.com>
2018-04-21 16:47:39 +0200

    added parameter tying

--- rnng/rnng.py
+++ rnng/rnng.py
@@ -978,7 +978,6 @@ class RNNGparser:
         print('Num NT actions          :',len(self.nonterminals),flush=True)
         print('Num struct actions      :',len(self.actions),flush=True)
         print('Lexicon size            :',len(self.rev_word_codes),flush=True)
-        print('Outer hidden layer size :',self.hidden_size,flush=True)
         print('Stack embedding size    :',self.stack_embedding_size,flush=True)
         print('Stack hidden size       :',self.stack_hidden_size,flush=True)
 
