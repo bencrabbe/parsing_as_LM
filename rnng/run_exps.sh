3a37cb12cd46a69c8fa2660b5f7d9972a338838a
bencrabbe <benoit.crabbe@gmail.com>
2018-04-22 10:21:49 +0200

    experimental support

--- rnng/run_exps.sh
+++ rnng/run_exps.sh
@@ -24,7 +24,8 @@ train_brown(){
    mkdir -p $NAME
    make_config "$NAME/$NAME" $1 $2 $3
    source activate py36
-   nohup python rnng.py -m $NAME/$NAME -t ptb_train_mrg -d -ptb_dev.mrg -b ptb-1000.brown -c "$NAME/$NAME.prm" >> $nohup.NAME.out & 
+   #nohup python rnng.py -m $NAME/$NAME -t ptb_train_mrg -d -ptb_dev.mrg -b ptb-1000.brown -c "$NAME/$NAME.prm" >> $nohup.NAME.out & 
+   python rnng.py -m $NAME/$NAME -t ptb_train_mrg -d -ptb_dev.mrg -b ptb-1000.brown -c "$NAME/$NAME.prm"
 }
 
 #train_lexicalized(){
