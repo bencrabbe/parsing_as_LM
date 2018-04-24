b826ff6aaf6b304e7d60894ab847badf580b54a0
bencrabbe <benoit.crabbe@gmail.com>
2018-04-24 15:49:15 +0200

    rnnlm comparable with the parser lm

--- rnng/lm_utils.py
+++ rnng/lm_utils.py
@@ -24,16 +24,8 @@ class RNNLMGenerator:
         self.batch_size             = batch_size
         self.batch_width            = batch_width
         self.make_exact_batches()
-
-        for elt in zip(self.X_stable,self.Y_stable):
-            print(elt)
-        
         self.make_batches(max_width=batch_width)
 
-        print()
-        for elt in zip(self.X_stable,self.Y_stable):
-            print(elt)
-        exit(0)
         
     def get_num_sentences(self):
         return len(self.X_stable)
