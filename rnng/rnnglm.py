03433f71452e6b88dd1aad5e22a3fbfb12098f0b
bencrabbe <benoit.crabbe@gmail.com>
2018-04-25 13:11:56 +0200

    rnnlm comparable with the parser lm

--- rnng/rnnglm.py
+++ rnng/rnnglm.py
@@ -347,7 +347,7 @@ if __name__ == '__main__':
     istream.close()
 
     rnnlm = RNNGlm(embedding_size=300,memory_size=300)
-    rnnlm.train_rnn_lm('testlm',train_treebank,dev_treebank,lr=0.001,dropout=0.3,batch_size=200,max_epochs=15,cls_filename='ptb-1000.brown',w2v_file=None)    
+    rnnlm.train_rnn_lm('testlm',train_treebank,dev_treebank,lr=0.0001,dropout=0.3,batch_size=200,max_epochs=15,cls_filename='ptb-1000.brown',w2v_file=None)    
 
 
 
