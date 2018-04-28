064d68a87eb0a10c700f2cc0d2c6f8f6806a8bdb
bencrabbe <benoit.crabbe@gmail.com>
2018-04-28 14:28:18 +0200

    improved lexical encoding for lm

--- rnng/rnnglm.py
+++ rnng/rnnglm.py
@@ -368,7 +368,7 @@ if __name__ == '__main__':
     istream.close()
 
     rnnlm = RNNGlm(embedding_size=300,memory_size=300)
-    rnnlm.train_rnn_lm('testlm',train_treebank,dev_treebank,lr=0.00001,dropout=0.5,batch_size=200,max_epochs=350,cls_filename='ptb-1000.brown',w2v_file='word_embeddings/w2v-ptb.txt')    
+    rnnlm.train_rnn_lm('testlm',train_treebank[:100],train_treebank[:100],lr=0.00001,dropout=0.5,batch_size=200,max_epochs=350,cls_filename='ptb-1000.brown',w2v_file='word_embeddings/w2v-ptb.txt')    
 
     #rnnlm.train_rnn_lm('testlm',train_treebank,dev_treebank,lr=0.001,dropout=0.3,batch_size=100,max_epochs=15,w2v_file='word_embeddings/w2v-ptb.txt')    
 
