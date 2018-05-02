b60eac07bb948b1d50b25f5c0a5acf71a0d9f5f1
bencrabbe <benoit.crabbe@gmail.com>
2018-05-02 22:11:38 +0200

    minor correction

--- rnng/rnng.py
+++ rnng/rnng.py
@@ -1161,7 +1161,7 @@ if __name__ == '__main__':
             idx  = 0
             for line in train_stream:
                 if idx >= 100:
-                train_treebank.append(ConsTree.read_tree(line))
+                    train_treebank.append(ConsTree.read_tree(line))
                 idx += 1
                 if idx >= 120:
                     break
