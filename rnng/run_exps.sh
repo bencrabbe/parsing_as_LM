897a89c9b0065c5d22aa83d3d597d1536322ce69
bencrabbe <benoit.crabbe@gmail.com>
2018-07-22 20:22:09 +0200

    class factored rnnlm

--- rnng/run_exps.sh
+++ rnng/run_exps.sh
@@ -53,6 +53,7 @@ train_rnnlm(){
     source activate py36
     nohup python rnnglmf.py -m $NAME/$NAME -t ptb_train.raw -d ptb_dev.raw -b ptb-250.brown -c "$NAME/$NAME.prm" > "nohup.$NAME.out" &   
 } 
+
 train_rnnlm 300 200 0.5
 train_rnnlm 300 300 0.5
 train_rnnlm 200 300 0.5
