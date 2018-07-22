0742ea70cdf8d8ffe0554942d01a8e944fff724a
bencrabbe <benoit.crabbe@gmail.com>
2018-07-22 15:03:39 +0200

    class factored lm

--- rnng/rnnglmf.py
+++ rnng/rnnglmf.py
@@ -11,7 +11,7 @@ from lexicons import *
 """
 That's a simple RNNLM designed to be interoperable with the rnng parser.
 Allows comparisons and sharing of parameters.
-** It is designed to run on a CPU **
+** It is designed to run mainly on a CPU **
 """
 
 class RNNGlm:
@@ -73,14 +73,15 @@ class RNNGlm:
             return dy.dropout(expr,self.dropout)
 
         
-    def train_rnn_lm(self,modelname,train_sentences,validation_sentences,lr=0.1,dropout=0.3,max_epochs=10):
+    def train_rnn_lm(self,modelname,train_sentences,validation_sentences,lr=0.1,dropout=0.3,max_epochs=10,batch_size=1):
         """
-        Trains an RNNLM on a data set. Vanilla (and slow) SGD training mode without batch or any funny optimization.
+        Trains an RNNLM on a data set. Vanilla SGD training mode with simple minibatching and without any funny optimization.
         @param modelname: a string used as prefix of output files
         @param train_sentences,validation_sentences: lists of strings
         @param lr: learning rate for SGD
         @param dropout: dropout
         @param max_epochs : number of epochs to run
+        @param batch_size : size of batches
         """
 
         self.code_lexicon(train_sentences)
@@ -89,40 +90,62 @@ class RNNGlm:
 
         trainer = dy.SimpleSGDTrainer(self.model,learning_rate=lr)
         min_nll = np.inf
+
+        ntrain_sentences = len(train_sentences)
+        ndev_sentences   = len(train_sentences)
         
         for e in range(max_epochs):
+
             NLL = 0
             N = 0
-            for sent in train_sentences:
+            
+            batches_processed = 0
+            bbegin = 0
+            
+            while bbegin < ntrain_sentences:
                 dy.renew_cg()
-                X          = [self.lexicon.index(word) for word  in [RNNGlm.START_TOKEN] + sent[:-1] ]
-                Y          = [self.lexicon.index(word) for word in sent]
-                state      = self.rnn.initial_state()
-                xinputs    = [dy.dropout(self.E[x],self.dropout) for x in X]
-                state_list = state.add_inputs(xinputs)
-                outputs    = [self.O.neg_log_softmax(dy.dropout(S.output(),self.dropout),y) for (S,y) in zip(state_list,Y) ]
+                outputs = []
+                bbegin = batches_processed * batch_size
+                bend = min(ntrain_sentences,bbegin + batch_size)
+                for sent in train_sentences[bbegin:bend]:
+                    X          = [self.lexicon.index(word) for word  in [RNNGlm.START_TOKEN] + sent[:-1] ]
+                    Y          = [self.lexicon.index(word) for word in sent]
+                    state      = self.rnn.initial_state()
+                    xinputs    = [dy.dropout(self.E[x],self.dropout) for x in X]
+                    state_list = state.add_inputs(xinputs)
+                    outputs.extend([self.O.neg_log_softmax(dy.dropout(S.output(),self.dropout),y) for (S,y) in zip(state_list,Y) ])
+                    N         += len(Y)
+
                 loc_nll    = dy.esum(outputs)
                 NLL       += loc_nll.value()
-                N         += len(Y)
                 loc_nll.backward()
                 trainer.update()
-                
+                batches_processed += 1
+                    
             print('[Training]   Epoch %d, NLL = %f, PPL = %f'%(e,NLL,np.exp(NLL/N)),flush=True)
 
             NLL = 0
             N = 0
-            for sent in validation_sentences:
+            
+            batches_processed = 0
+            bbegin = 0
+            while bbegin < ntrain_sentences:
                 dy.renew_cg()
-                X          = [self.lexicon.index(word) for word  in [RNNGlm.START_TOKEN] + sent[:-1] ]
-                Y          = [self.lexicon.index(word) for word in sent]
-                state      = self.rnn.initial_state()
-                xinputs    = [self.E[x] for x in X]
-                state_list = state.add_inputs(xinputs)
-                outputs    = [self.O.neg_log_softmax(S.output(),y) for (S,y) in zip(state_list,Y) ]
+                outputs = []                
+                bbegin = batches_processed * batch_size
+                bend = min(ntrain_sentences,bbegin + batch_size)
+                for sent in validation_sentences[bbegin:bend]:
+                    X          = [self.lexicon.index(word) for word  in [RNNGlm.START_TOKEN] + sent[:-1] ]
+                    Y          = [self.lexicon.index(word) for word in sent]
+                    state      = self.rnn.initial_state()
+                    xinputs    = [self.E[x] for x in X]
+                    state_list = state.add_inputs(xinputs)
+                    outputs.extend([self.O.neg_log_softmax(S.output(),y) for (S,y) in zip(state_list,Y) ])
+                    N         += len(Y)
                 loc_nll    = dy.esum(outputs)
                 NLL       += loc_nll.value()
-                N         += len(Y)
-                
+                batches_processed += 1
+ 
             print('[Validation] Epoch %d, NLL = %f, PPL = %f\n'%(e,NLL,np.exp(NLL/N)),flush=True)
 
 
