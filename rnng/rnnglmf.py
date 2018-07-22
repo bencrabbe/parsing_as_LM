9439301a77c5383c2c0d8ecdeb69f1da40170935
bencrabbe <benoit.crabbe@gmail.com>
2018-07-22 18:56:28 +0200

    class factored rnnlm

--- rnng/rnnglmf.py
+++ rnng/rnnglmf.py
@@ -214,11 +214,11 @@ def read_config(filename=None):
     config.read(filename)
 
     params = {}
-    params['structure']['embedding_size'] = int(config['structure']['embedding_size']) if 'embeddings' in config['structure'] else 100
-    params['structure']['memory_size']    = int(config['structure']['memory_size'])    if 'memory_size' in config['structure'] else 100
-    params['learning']['dropout']         = float(config['structure']['dropout'])      if 'dropout' in config['learning'] else 0.1
-    params['learning']['learning_rate']   = float(config['structure']['learning_rate'])if 'learning_rate' in config['learning'] else 0.1
-    params['learning']['num_epochs']      = int(config['structure']['num_epochs'])     if 'num_epochs' in config['learning'] else 20
+    params['embedding_size'] = int(config['structure']['embedding_size']) if 'embeddings' in config['structure'] else 100
+    params['memory_size']    = int(config['structure']['memory_size'])    if 'memory_size' in config['structure'] else 100
+    params['dropout']         = float(config['structure']['dropout'])      if 'dropout' in config['learning'] else 0.1
+    params['learning_rate']   = float(config['structure']['learning_rate'])if 'learning_rate' in config['learning'] else 0.1
+    params['num_epochs']      = int(config['structure']['num_epochs'])     if 'num_epochs' in config['learning'] else 20
     return params
 
 
@@ -271,8 +271,8 @@ if __name__ == '__main__':
 
         if config_file:
             config = read_config(config_file)
-            rnnlm = RNNGlm(brown_file,embedding_size=config["structure"]['embedding_size'] ,memory_size=config["structure"]['memory_size'])
-            rnnlm.train_rnn_lm(model_name,train_treebank,dev_treebank,lr=config['learning']['learning_rate'],dropout=config['learning']['dropout'],max_epochs=config['learning']['num_epochs'],batch_size=32)
+            rnnlm = RNNGlm(brown_file,embedding_size=config['embedding_size'] ,memory_size=config['memory_size'])
+            rnnlm.train_rnn_lm(model_name,train_treebank,dev_treebank,lr=config['learning_rate'],dropout=config['dropout'],max_epochs=config['num_epochs'],batch_size=32)
         else:
             rnnlm = RNNGlm(brown_file,embedding_size=100 ,memory_size=100)
             rnnlm.train_rnn_lm(model_name,train_treebank,dev_treebank,lr=0.1,dropout=0.5,max_epochs=40,batch_size=32)
