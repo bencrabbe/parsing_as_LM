614cdcd271886e8b00eadf352ceb0a8d0495dd79
bencrabbe <benoit.crabbe@gmail.com>
2018-07-22 18:44:13 +0200

    class factored rnnlm

--- rnng/rnnglmf.py
+++ rnng/rnnglmf.py
@@ -210,9 +210,8 @@ def read_config(filename=None):
     Return an hyperparam dictionary
     """
     import configparser
-
-    
     config = configparser.ConfigParser()
+    print('*',filename,"*")
     config.read(filename)
     print('*',filename,"*",config)
     config['structure']['embedding_size'] = int(config['structure']['embedding_size']) if 'embeddings' in config['structure'] else 100
