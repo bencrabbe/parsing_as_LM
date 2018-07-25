ccd471f6356049513737c386c4dd59c1e6e80865
bencrabbe <benoit.crabbe@gmail.com>
2018-07-25 09:57:18 +0200

    batched optimization

--- rnng/rnngf.py
+++ rnng/rnngf.py
@@ -646,4 +646,4 @@ if __name__ == '__main__':
     dev_stream.close()
      
     parser = RNNGparser('ptb-250.brown')
-    parser.train_model(train_treebank,dev_treebank,'test_rnngf/test_rnngf',epochs=20,batch=32)
+    parser.train_model(train_treebank,dev_treebank,'test_rnngf/test_rnngf',epochs=20,batch_size=32)
