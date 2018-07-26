8632ca3e79fe2028717cb2278ae188815aeaf500
bencrabbe <benoit.crabbe@gmail.com>
2018-07-26 14:44:27 +0200

    beam search

--- rnng/rnngf.py
+++ rnng/rnngf.py
@@ -882,7 +882,7 @@ class RNNGparser:
             argmax_tree        = RNNGparser.deriv2tree(argmax_derivation)
             argmax_tree.expand_unaries() 
             print(argmax_tree,file=ostream,flush=True)
-
+            exit(0)
             
 if __name__ == '__main__':
 
