7b33a43ec62f4e8b59660d5e32bd0bb574f15780
bencrabbe <benoit.crabbe@gmail.com>
2018-07-26 15:57:12 +0200

    beam search

--- rnng/rnngf.py
+++ rnng/rnngf.py
@@ -906,7 +906,7 @@ if __name__ == '__main__':
 
     parser = RNNGparser.load_model('test_rnngf/test_rnngf_gpu')
     test_stream   = open('ptb_test.raw')
-    parser.parse_corpus(test_stream,sys.stderr,K=100)
+    parser.parse_corpus(test_stream,sys.stderr,K=10)
     test_stream.close()
 
     
