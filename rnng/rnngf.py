627a4630bbb51ff227303c16cc0527cca1d693eb
bencrabbe <benoit.crabbe@gmail.com>
2018-07-30 14:38:34 +0200

    added stats indicators

--- rnng/rnngf.py
+++ rnng/rnngf.py
@@ -1085,7 +1085,7 @@ class RNNGparser:
                     NLL += nll
                     N   += len(tokens)
                     if stats_stream:
-                        dataframe.to_csv(stats_stream,header=stats_header,mode='a'),flush=True)
+                        dataframe.to_csv(stats_stream,header=stats_header,mode='a')
                         stats_header = False
                 else:
                     print('(())')
