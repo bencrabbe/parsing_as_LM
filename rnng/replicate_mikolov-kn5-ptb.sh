ec7f94b67b22dea6393ecc0cbd24070c01e60f53
bencrabbe <benoit.crabbe@gmail.com>
2018-07-31 12:53:28 +0200

    made KN5 comparable

--- rnng/replicate_mikolov-kn5-ptb.sh
+++ rnng/replicate_mikolov-kn5-ptb.sh
@@ -5,8 +5,17 @@ export PATH=$PATH:$PWD/srilm/bin/i686-m64
 
 ORD=5
 
-ngram-count -order $ORD -text ptb_train.raw -lm templm -kndiscount -interpolate -unk -gt3min 1 -gt4min 1
+python lexicons.py ptb_train.raw ptb_train.raw > ptb_train.unk
+python lexicons.py ptb_train.raw ptb_dev.raw > ptb_test.unk
+python lexicons.py ptb_train.raw ptb_test.raw > ptb_test.unk
 
 
-ngram -ppl ptb_dev.raw -lm templm -order $ORD -unk
-ngram -ppl ptb_test.raw -lm templm -order $ORD -unk
+ngram-count -order $ORD -text ptb_train.unk -lm templm -kndiscount -interpolate -unk -gt3min 1 -gt4min 1
+
+
+ngram -ppl ptb_dev.unk -lm templm -order $ORD -unk
+ngram -ppl ptb_test.unk -lm templm -order $ORD -unk
+
+rm ptb_train.unk
+rm ptb_dev.unk
+rm ptb_test.unk
