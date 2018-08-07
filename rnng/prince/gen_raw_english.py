9a598c444c317fcbd8f864cb8b23059c4262bd81
bencrabbe <benoit.crabbe@gmail.com>
2018-08-07 10:35:22 +0200

    gen prince

--- rnng/prince/gen_raw_english.py
+++ rnng/prince/gen_raw_english.py
@@ -1,6 +1,6 @@
 from constree import *
 
-istream = open('prince/little-prince-english-stanford.txt')
+istream = open('little-prince-english-stanford.txt')
 
 for line in istream:
     print(' '.join(ConsTree.read_tree(line).tokens()))
