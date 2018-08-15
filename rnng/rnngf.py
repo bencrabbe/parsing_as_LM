29507c05ceb9eced0fc30bbddf381d46e6583ba2
bencrabbe <benoit.crabbe@gmail.com>
2018-08-15 11:58:05 +0200

    added particle beam search

--- rnng/rnngf.py
+++ rnng/rnngf.py
@@ -947,7 +947,7 @@ class RNNGparser:
             elt.K = round(K * weight)
             if elt.K > 0.0:
               beam.append(elt)
-              
+          print('#Beam',len(beam))
           #search
           nextword = []
           while beam:
