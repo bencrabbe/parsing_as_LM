302580a9590b8879e1d1ce3b4ca7e848cf4dd27d
bencrabbe <benoit.crabbe@gmail.com>
2018-08-16 09:54:44 +0200

    added particle beam search

--- rnng/rnngf.py
+++ rnng/rnngf.py
@@ -10,7 +10,7 @@ import getopt
 import json
 
 from random        import shuffle
-from math          import exp,log,ceil
+from math          import exp,log,floor
 from constree      import *
 from lexicons      import *
 from proc_monitors import *
