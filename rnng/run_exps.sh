519e7526fa1c13a39307a7a64279e07e7172ffae
bencrabbe <benoit.crabbe@gmail.com>
2018-08-25 11:59:24 +0200

    changed parser hyperparams

--- rnng/run_exps.sh
+++ rnng/run_exps.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 
 MKL_NUM_THREADS=6
-NUM_EPOCHS=30
 
 make_config(){
     
-    #$1 NAME $2 = stack embedding size , $3 = lstm memory size $4 = word embedding size $5 = dropout
+    #$1 NAME $2 = word embedding size  $3 = lstm memory size $4 = dropout
     CNAME="$1.conf"
     echo > $CNAME  
     echo "[structure]" >> $CNAME
-    echo "stack_embedding_size = $2" >> $CNAME
     echo "stack_hidden_size    = $3" >> $CNAME
-    echo "word_embedding_size" = $4  >> $CNAME
+    echo "word_embedding_size" = $2  >> $CNAME
     echo "char_embedding_size" = 50  >> $CNAME
     echo "char_hidden_size"    = 50  >> $CNAME
 
     echo >> $CNAME
     echo "[learning]"                  >> $CNAME
-    echo "num_epochs    = $NUM_EPOCHS" >> $CNAME
-    echo "batch_size    = 1"          >> $CNAME
+    echo "num_epochs    = 25"          >> $CNAME
+    echo "batch_size    = 1"           >> $CNAME
     echo "learning_rate = 0.1"         >> $CNAME
-    echo "dropout       = $5"          >> $CNAME
+    echo "dropout       = $4"          >> $CNAME
 }
 
 make_lmconfig(){
@@ -37,10 +35,10 @@ make_lmconfig(){
 }
 
 train_rnng(){
-   # $1 = stack embedding size , $2 = lstm memory size $3 = word embedding size $4 = dropout
-   NAME="brown-$1-$2-$3-250"  #250 stands for the number of clusters
+   # $1 = word embedding size $2 = lstm memory size $3 = dropout
+   NAME="rnng-$1-$2-$3" 
    mkdir -p $NAME
-   make_config "$NAME/$NAME" $1 $2 $3 $4 
+   make_config "$NAME/$NAME" $1 $2 $3 
    source activate py36
    nohup python rnngf.py -m $NAME/$NAME -t ptb_train.mrg -d ptb_dev.mrg -b ptb-250.brown -c "$NAME/$NAME.conf" -p ptb_test.mrg -s -B 10000 > "nohup.$NAME.out"  &
    #python rnngf.py -m $NAME/$NAME -p prince/prince.en.txt -s >> "nohup.$NAME.out") &
