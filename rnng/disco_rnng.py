032e4318fe11e0d5bf54643cda515b433d9cb16b
bencrabbe <benoit.crabbe@gmail.com>
2019-02-05 21:17:54 +0100

    added generative model

--- rnng/disco_rnng.py
+++ rnng/disco_rnng.py
@@ -1169,14 +1169,14 @@ class DiscoRNNGparser:
                 
 if __name__ == '__main__':
     
-    #p       = DiscoRNNGparser(config_file='disco_negra_model/default.conf')
-    #tstream = open('negra/train.mrg') 
-    #dstream = open('negra/dev.mrg')
-    #p.train_model(tstream,dstream,'disco_negra_model/negra_model',config_file='default.conf',generative=False)
-    #tstream.close()
-    #dstream.close()
+    p       = DiscoRNNGparser(config_file='disco_negra_model/default.conf')
+    tstream = open('negra/train.mrg') 
+    dstream = open('negra/dev.mrg')
+    p.train_model(tstream,dstream,'disco_negra_model/negra_model',config_file='default.conf',generative=False)
+    tstream.close()
+    dstream.close()
 
-    p = DiscoRNNGparser.load_model('disco_negra_model/negra_model')
+    #p = DiscoRNNGparser.load_model('disco_negra_model/negra_model')
     
     pstream = open('negra/test.mrg')
     pred_stream = open('disco_negra_model/pred_test.mrg','w')
