8b59dd7a8058b79c9f6c3f03d94112ac60adaf3b
bencrabbe <benoit.crabbe@gmail.com>
2019-02-06 12:13:31 +0100

    script for brown clusters

--- rnng/discbracket2raw.py
+++ rnng/discbracket2raw.py
@@ -6,7 +6,7 @@ ostream = open("negra/train.raw")
 for line in istream:
 
     t = DiscoTree.read_tree(line)
-    print(' '.join(t.words(),file=ostream)
+    print(' '.join(t.words(),file=ostream))
 
 istream.close()
 ostream.close()
