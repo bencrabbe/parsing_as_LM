c52abd023326de88582866a16e32421a78c9f069
bencrabbe <benoit.crabbe@gmail.com>
2019-05-02 09:46:50 +0200

    reframed importance distribution

--- rnng/rnngf.py
+++ rnng/rnngf.py
@@ -1094,7 +1094,7 @@ class RNNGparser:
          #select
           beam.clear()
           #weights = [ exp(elt.prefix_gprob)**alpha for elt in nextword[-1] ]
-          weights = [ exp(elt.prefix_gprob - elt.prefix_dprob)**alpha for elt in nextword[-1] ]
+          weights = [ (elt.K * exp(elt.prefix_gprob - elt.prefix_dprob))**alpha for elt in nextword[-1] ]
           Z       = sum(weights)
           weights = [w/Z for w in weights]
           for elt,weight in zip(nextword[-1],weights):
