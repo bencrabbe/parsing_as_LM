90ab91bb0bf448889f55090b193c8947759ede66
bencrabbe <benoit.crabbe@gmail.com>
2019-05-17 17:36:45 +0200

    restored abse

--- rnng/simple_rnnlm.py
+++ rnng/simple_rnnlm.py
@@ -167,7 +167,7 @@ class RNNLM:
             self.model   = self.model.populate('rnnlm_model.prm')
             self.dropout = 0.0 
              
-lm = RNNLM('ptb-250.brown',word_embedding_size=300,hidden_size=200) 
+lm = RNNLM('ptb-250.brown',word_embedding_size=300,hidden_size=300) 
 lm.train_rnnlm('ptb_train.mrg','ptb_dev.mrg',max_epochs=20,lr=0.1,dropout=0.7)
 print('WSJ PPL',lm.eval_dataset('ptb_test.mrg')[1])
 print('Prince PPL',lm.eval_dataset('prince/prince.en.txt',strip_trees=False)[1])
