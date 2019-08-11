f42e85fbcb1ee6639928ba40dd6772f671fb6cc2
bencrabbe <benoit.crabbe@gmail.com>
2019-08-11 12:44:30 +0200

    history based parser

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -678,7 +678,7 @@ class LCmodel(nn.Module):
         lex_loss           = nn.NLLLoss(ignore_index=train_set.lex_vocab.stoi[train_set.pad])
         struct_loss        = nn.NLLLoss(ignore_index=train_set.struct_vocab.stoi[train_set.pad])
         
-        optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        optimizer = optim.SGD(self.parameters(), lr=learning_rate)
         
         for e in range(epochs):
             L = 0
@@ -820,4 +820,4 @@ if __name__ == '__main__':
     dev_df         = ParsingDataSet(devset,root_dataset=train_df)    
     parser = LCmodel(train_df,rnn_memory=300,embedding_size=300,device=1)
     parser.cuda(device=1)
-    parser.train(train_df,dev_df,40,batch_size=64,learning_rate=0.001,device=1,alpha=1.0) 
+    parser.train(train_df,dev_df,40,batch_size=64,learning_rate=0.1,device=1,alpha=1.0) 
