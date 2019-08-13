24b0528eeec6860a54d7539e2660fd4852493a10
bencrabbe <benoit.crabbe@gmail.com>
2019-08-13 09:20:33 +0200

    history based parser

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -844,4 +844,4 @@ if __name__ == '__main__':
     
     parser = LCmodel(dev_df,rnn_memory=300,embedding_size=100,device=3)
     parser.cuda(device=3)
-    parser.train(dev_df,dev_df,200,batch_size=32,learning_rate=0.1,device=3,alpha=0.0) 
+    parser.train(dev_df,dev_df,200,batch_size=32,learning_rate=1.0,device=3,alpha=0.0) 
