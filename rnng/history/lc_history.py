7f73d8f94c7ebf89baf4d2e61ad6be6b4a8e310d
bencrabbe <benoit.crabbe@gmail.com>
2019-08-13 12:53:56 +0200

    history based parser

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -866,7 +866,7 @@ if __name__ == '__main__':
 
     #train_df       = ParsingDataSet(trainset,min_lex_counts=10)
     #dev_df         = ParsingDataSet(devset,root_dataset=train_df)
-    dev_df         = ParsingDataSet(devset)
+    dev_df         = ParsingDataSet([ConsTree.read_tree(t) for t in devset])
     #print('Train Vocab size',train_df.lex_vocab.size())
     print('Dev   Vocab size',dev_df.lex_vocab.size())
     #print('Train label size',train_df.struct_vocab.size())
