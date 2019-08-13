276b96627fb455afb2839204358ea780858db0fc
bencrabbe <benoit.crabbe@gmail.com>
2019-08-13 09:59:13 +0200

    history based parser

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -866,6 +866,6 @@ if __name__ == '__main__':
     #print('Train label size',train_df.struct_vocab.size())
     print('Dev label size',dev_df.struct_vocab.size(),dev_df.struct_vocab.itos)
     
-    parser = LCmodel(dev_df,rnn_memory=600,embedding_size=100,device=3)
+    parser = LCmodel(dev_df,rnn_memory=300,embedding_size=100,device=3)
     parser.cuda(device=3)
     parser.train(dev_df,dev_df,200,batch_size=1,learning_rate=1.0,device=3,alpha=0.0) 
