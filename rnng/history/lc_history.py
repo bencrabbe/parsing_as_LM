a8d51ffb49e90a5eab1c5159b0956690a9c6179e
bencrabbe <benoit.crabbe@gmail.com>
2019-08-16 17:52:14 +0200

    fixing decoding bug

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -872,4 +872,4 @@ if __name__ == '__main__':
     
     parser = LCmodel(dev_df,rnn_memory=100,embedding_size=100,device=3)
     parser.cuda(device=3)
-    parser.train(dev_df,dev_df,400,batch_size=1,learning_rate=1.0,device=3,alpha=0.0) 
+    parser.train(dev_df,dev_df,400,batch_size=4,learning_rate=1.0,device=3,alpha=0.0) 
