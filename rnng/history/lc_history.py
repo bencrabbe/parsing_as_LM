9f9651f969860c5328e0e464ca32aef48e74d010
bencrabbe <benoit.crabbe@gmail.com>
2019-08-18 15:44:03 +0200

    fixing decoding bug

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -692,7 +692,7 @@ class LCmodel(nn.Module):
         lex_loss           = nn.NLLLoss(reduction='sum',ignore_index=train_set.lex_vocab.stoi[train_set.pad])
         struct_loss        = nn.NLLLoss(reduction='sum',ignore_index=train_set.struct_vocab.stoi[train_set.pad])
         #reduction='mean'
-        optimizer = optim.SGD(self.parameters(), lr=learning_rate)
+        optimizer = optim.SGD(self.parameters())#, lr=learning_rate)
         #scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
         scheduler = LambdaLR(optimizer,lr_lambda = lambda epoch:learning_rate)#/(1+0.5*epoch))
 
