0bcbf5aa1070ba30eabd1edc14fc3413cb0af8db
bencrabbe <benoit.crabbe@gmail.com>
2019-08-18 15:09:42 +0200

    fixing decoding bug

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -580,8 +580,8 @@ class LCmodel(nn.Module):
                     ntaction[ predict_c ] = np.NINF
                 #if d > r+1:
                 #    ntaction[ predict_c ]  = np.NINF
-                #if d == 1:
-                #    ntaction[ attach_c ]  = np.NINF
+                if d == 1:
+                    ntaction[ attach_c ]  = np.NINF
                 #decision
                 #print('ntlabel',list(zip(self.ref_set.struct_vocab.itos,np.exp(ntlabel))))
                 #print(idx,'struct_action',list(zip(self.ref_set.struct_action_vocab.itos,np.exp(ntaction))))
@@ -605,7 +605,7 @@ class LCmodel(nn.Module):
                 laction[ shift_attach_c ] = np.NINF
             if d > 0 and Stack[-1].is_complete(): 
                 laction[ shift_init_c ] = np.NINF
-            if d > r+1: 
+            if d >= r+1: 
                 laction[ shift_init_c ] = np.NINF
             #decision 
             ytoken,lex_action = decode_lexical(np.argmax(token), np.argmax(laction)) #pick the relevant prob for the token here ! (to be reworked)
