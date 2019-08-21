5f8469883b69ec6086518438ff3d23f8eb384f1c
bencrabbe <benoit.crabbe@gmail.com>
2019-08-21 11:04:32 +0200

    fixing decoding bug

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -851,9 +851,10 @@ if __name__ == '__main__':
     #trainset = list( input_treebank('../ptb_train.mrg') )
     #devset   = list( input_treebank('../ptb_dev.mrg') )
 
-    train_df       = ParsingDataSet(trainset,min_lex_counts=1)
-    dev_df         = ParsingDataSet(devset,root_dataset=train_df)
-    #dev_df         = ParsingDataSet([ConsTree.read_tree(t) for t in devset])
+    #train_df       = ParsingDataSet(trainset,min_lex_counts=1)
+    #dev_df         = ParsingDataSet(devset,root_dataset=train_df)
+    train_df       = ParsingDataSet([ConsTree.read_tree(t) for t in trainset])
+    dev_df         = ParsingDataSet([ConsTree.read_tree(t) for t in devset])
     print('Train Vocab size',train_df.lex_vocab.size())
     print('Dev   Vocab size',dev_df.lex_vocab.size())
     #print('Train label size',train_df.struct_vocab.size())
