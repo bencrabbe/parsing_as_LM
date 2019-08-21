765b62bcec55175dda87ac7bc0cfc83e51d9ead3
bencrabbe <benoit.crabbe@gmail.com>
2019-08-21 12:14:41 +0200

    fixing decoding bug

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -860,7 +860,6 @@ if __name__ == '__main__':
     #print('Train label size',train_df.struct_vocab.size(),train_df.struct_vocab.itos)
     
     parser = LCmodel(train_df,rnn_memory=1200,embedding_size=300,device=3)
-    parser.cuda(device=3) 
-    parser.train(train_df,dev_df,400,batch_size=32,learning_rate=0.001,device=3,alpha=0.0)  
+    parser.cuda(device=3)  
+    parser.train(train_df,dev_df,400,batch_size=32,learning_rate=0.01,device=3,alpha=0.0)  
 
-    #Use ReduceLR on Plateau with *0.1 increment and LR = 0.001 
