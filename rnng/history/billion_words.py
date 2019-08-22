cf9b0b738cad9ee3793568fbe3f5d5f37c5a4911
bencrabbe <benoit.crabbe@gmail.com>
2019-08-22 09:45:36 +0200

    lm mods

--- rnng/history/billion_words.py
+++ rnng/history/billion_words.py
@@ -1,21 +1,54 @@
 #!/usr/bin/env python
 
 import os
-
+import random
+from collections import Counter
 """
 This is a module dedicated to preprocess and extract stats from the 1 billion word corpus.
 """
 
 def process_files(root_path):
     """
+    Generator function.
     Yields billion words train filenames given the root path
     Args:
        root_path (string): the dirpath to the root of the billion words
     corpus (the README is in this dirpath)
     """
-    train_path = '/'.join([ root_path,'training-monolingual.tokenized.shuffled']) 
+    train_path = '/'.join( [ root_path,'training-monolingual.tokenized.shuffled']) 
     for rel_file in os.listdir(train_path):
         yield os.path.join(train_path,rel_file)
-        
 
-print(list(process_files('/home/bcrabbe/parsing_as_LM/rnng/history/billion_words')))
+def sample_file(root_path):
+    """
+    Samples a file name from the 1 billion words filenames
+    Args:
+       root_path (string): the dirpath to the root of the billion words
+    corpus
+    Yields : an absolute filename (as a string)
+    """ 
+    file_list = list(process_files(root_path))
+    yield random.choice(file_list)
+    
+
+def normalise_token(token):
+    return token
+
+def next_sentence(filename):
+    """
+    Generator function.
+    Yields the next sentence from filename or returns None if file is
+    empty
+    Returns.
+       a list of strings. The tokenized sentence.
+    """
+    istream = open(filename)
+    for sentence in istream:
+        yield [normalise_token(token) for token in sentence.split()]
+    istream.close()
+    return None
+    
+for filename in process_files('/home/bcrabbe/parsing_as_LM/rnng/history/billion_words'):
+    for sentence in next_sentence(filename):
+        print(sentence)
+    break
