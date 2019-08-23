83ef88c77e7222dbd6648400861d682d1beb6bf5
bencrabbe <benoit.crabbe@gmail.com>
2019-08-23 10:23:46 +0200

    ext vocab

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -916,7 +916,7 @@ if __name__ == '__main__':
     print('Dev   Vocab size',dev_df.lex_vocab.size())
     #print('Train label size',train_df.struct_vocab.size())
     #print('Train label size',train_df.struct_vocab.size(),train_df.struct_vocab.itos)
-    
+    exit(0)
     parser = LCmodel(train_df,rnn_memory=600,embedding_size=50,device=3)
     parser.cuda(device=3)  
     parser.train(train_df,dev_df,400,batch_size=32,learning_rate=0.001,device=3,alpha=1.0)  
