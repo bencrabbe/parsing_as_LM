81904b7bb1ec983da7fd893a9666f884184b4e21
bencrabbe <benoit.crabbe@gmail.com>
2019-08-23 10:45:42 +0200

    lm mods

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -898,8 +898,9 @@ def output_treebank(treelist,filename=None):
 
 if __name__ == '__main__':
 
-    #vocab = Vocabulary(extract_vocabulary('/home/bcrabbe/parsing_as_LM/rnng/history/billion_words'),unk='<unk>',sos='<sos>',min_freq=50)
-    #vocab.save('toto')
+    #Max vocab size with float64
+    vocab = Vocabulary(extract_vocabulary('/home/bcrabbe/parsing_as_LM/rnng/history/billion_words'),unk='<unk>',sos='<sos>',min_freq=100)
+    vocab.save('toto')
     evocab = Vocabulary.load('toto')
     
     #trainset   =  [ '(TOP@S I (S: (VP love (NP em both)) .))','(S (DP The (NP little monkey)) (VP screams loud))','(S (NP the dog) walks)','(S (NP a cat) (VP chases (NP the mouse)))','(S (NP A wolf) (VP eats (NP the pig)))']
