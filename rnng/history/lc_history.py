654bf52541869e357a523eefe326bd80d5401722
bencrabbe <benoit.crabbe@gmail.com>
2019-08-25 10:30:44 +0200

    lm mods

--- rnng/history/lc_history.py
+++ rnng/history/lc_history.py
@@ -1056,7 +1056,7 @@ if __name__ == '__main__':
     
     #parser = LCmodel(train_df,rnn_memory=600,embedding_size=300,device=0)
     parser = LCmodel.load('def12',device=0)
-    print(parser.eval_language_model(lm_df,batch_size=32,device=0)) 
+    print(parser.eval_language_model(dev_df,batch_size=32,device=0)) 
     exit(0)
 
     parser.train_language_model(lm_df,dev_df,1,batch_size=32,learning_rate=0.001,device=0,alpha=0.0,save_path="def12")
