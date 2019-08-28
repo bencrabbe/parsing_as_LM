b6cab4d81fde33f7535939aa74efb336f033ead6
bencrabbe <benoit.crabbe@gmail.com>
2019-08-28 21:42:13 +0200

    changed marginals

--- rnng/rnngf.py
+++ rnng/rnngf.py
@@ -929,7 +929,7 @@ class RNNGparser:
 
             loc_nll2            = (marginal_prob - stats_dic['prefix_logprob'])   
             nll2               += loc_nll2
-            print(token,loc_nll2)
+            print(token,loc_nll2,nll2,idx)
             
             marginal_prob       = stats_dic['prefix_logprob']
             prefix_entropy      = stats_dic['entropy']
