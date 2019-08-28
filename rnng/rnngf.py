505cc5b931beabbb0103800a6763b2f87bcb8f3e
bencrabbe <benoit.crabbe@gmail.com>
2019-08-28 19:59:15 +0200

    changed marginals

--- rnng/rnngf.py
+++ rnng/rnngf.py
@@ -884,7 +884,7 @@ class RNNGparser:
         #Log likelihood etc...
         #logprobs2              = [elt.prefix_gprob/np.log(2) for elt in success] #change logprobs from base e to base 2
         logprobs2              = [(elt.prefix_gprob-elt.prefix_dprob+np.log(elt.K))/np.log(2) for elt in success] 
-        actualK                = sum(elt.K for elt in success)
+        actualK                = sum(elt.trueK for elt in success)
         print(actualK)
         marginal_logprob2      = np.logaddexp2.reduce(logprobs2) - np.log2(actualK)  
         cond_logprobs2         = [elt-marginal_logprob2 for elt in logprobs2]
